d551b2813a98be6be67eb378dfc48e8defdfe6ce
“Sourav <duttasourav029@gmail>
2020-04-22 21:23:15 +0530

    navbar and front page

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{asset('img/link.png')}}" alt="link logo">BacklinkWeb
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link call-to-action" href="/">Submit Your Domain</a>
+                        </li>                    
+                    </ul>
+                </div>
+            </div>
+        </nav>
